8ecdfe95b135dc674c58034ca2ce1c040f879628
shiblykt <shiblykt56@gmail.com>
2022-05-02 22:28:41 +0530

    farmcorps app

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,16 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
+      home: Scaffold(backgroundColor:Colors.blue.shade100,
         appBar: AppBar(
-          title: Text("Farmcorps"),
+        title: Text("Farmcorps"),
           centerTitle: true,
+            backgroundColor:Colors.green,
         ),
+        body:Center(child:Image(
+          image: AssetImage('images/diamond.png'),
+        ),)
       ),
     ),
   );
-}
+} 
